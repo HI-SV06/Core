1472f555bde99ba66d8911c5da8a8dc4a0feb07d
Shishir-vardhan <shishirvardhan695@gmail.com>
2025-01-17 21:15:13 +0530

    Create Middleware Component in Program.cs

--- Core/Program.cs
+++ Core/Program.cs
@@ -1,6 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+app.Use(async (Context, next) =>
+{
+    if (Context.Request.Method == HttpMethods.Get && Context.Request.Query["Custom"] == "True")
+    {
+        Context.Response.ContentType = "text/plain";
+        await Context.Response.WriteAsync("Custom Middleware \n");
+    }
+
+    await next();
+});
+
 app.MapGet("/", () => "Hello World!");
 app.MapGet("/hello", () => "Welcome Shishir to .Net world");
 
