51f7775d01d2c9a000aac6d1ef988580953020d4
Shishir-vardhan <shishirvardhan695@gmail.com>
2025-01-18 02:32:51 +0530

    Modifying the http response onbject after the next function is called using middleware component

--- Core/Program.cs
+++ Core/Program.cs
@@ -3,22 +3,12 @@ using Core;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-/*
- * Output ->
- * a. if url is localhost:3000/short then if condition will work and Request short-circuited display on browser.
- * b. if url is differ from above then else condition run & bcoz of await next(), the next middleware component get's called.
- */
+
 
 app.Use(async (context, next) =>
 {
-    if (context.Request.Path == "/short")
-    {
-        await context.Response.WriteAsync("Request short-circuited");
-    }
-    else
-    {
-        await next();
-    }
+    await next();
+    await context.Response.WriteAsync($"\n Status Code : {context.Response.StatusCode}");
 });
 
 app.UseMiddleware<Middleware>();
