9e923bc1ffb360ae93b74545f790dade5e31aaab
Shishir-vardhan <shishirvardhan695@gmail.com>
2025-01-20 02:42:36 +0530

    Creating endpoint to fetch specific record based on Id.

---
+++ Core/Data/SeedData.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Core.Data
+{
+    public class SeedData
+    {
+        public static void SeedDatabase(DataContext context)
+        {
+            //Below code Ensures that the latest database migrations are applied to the database.
+            context.Database.Migrate();
+
+            if (context.Products.Count() == 0 && context.Categories.Count() == 0)
+            {
+                //Creates two new Category objects with names "fruits" and "shirts".
+                Category fruits = new Category { Name = "fruits" };
+                Category shirts = new Category { Name = "shirts" };
+
+                /*
+                 * context.Products.AddRange(...) =>Adds new Product objects to the Products table.
+                 */
+                context.Products.AddRange(
+                    new Product
+                    {
+                        Name = "Apples",
+                        Price = 2.5M,
+                        Category = fruits,
+                    },
+                    new Product
+                    {
+                        Name = "Banaa",
+                        Price = 3.5M,
+                        Category = fruits,
+                    },
+                    new Product
+                    {
+                        Name = "Grapes",
+                        Price = 5.5M,
+                        Category = fruits,
+                    },
+                    new Product
+                    {
+                        Name = "Pineapple",
+                        Price = 6.0M,
+                        Category = fruits,
+                    },
+                    new Product
+                    {
+                        Name = "Back Shirt",
+                        Price = 15.5M,
+                        Category = shirts,
+                    },
+                    new Product
+                    {
+                        Name = "Orange Shirt",
+                        Price = 16.5M,
+                        Category = shirts,
+                    },
+                    new Product
+                    {
+                        Name = "Blue Shirt",
+                        Price = 10.5M,
+                        Category = shirts,
+                    },
+                    new Product
+                    {
+                        Name = "White Shirt",
+                        Price = 15.5M,
+                        Category = shirts,
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
--- Core/Program.cs
+++ Core/Program.cs
@@ -2,6 +2,7 @@ using Core.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 using System.Data.Common;
+using System.Text.Json;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +13,73 @@ builder.Services.AddDbContext<DataContext>(Options =>
 
 var app = builder.Build();
 
+
+//Creating a web service/ web endpoint
+
+
+//Getting the product based on id from database
+const string BASEURL = "/api/products";
+
+app.MapGet($"{BASEURL}/{{id}}", async (HttpContext context, DataContext data) =>
+{
+    // To know more what is RoutValue check end of this page
+    string id = context.Request.RouteValues["id"] as string;
+    if(id != null)
+    {
+        Product product = data.Products.Find(long.Parse(id));
+        if (product == null)
+        {
+            context.Response.StatusCode = StatusCodes.Status404NotFound;
+        }
+        else
+        {
+            context.Response.ContentType = "application/json";
+            await context.Response.WriteAsync(JsonSerializer.Serialize<Product>(product));
+        }
+    }
+    else
+    {
+        context.Response.StatusCode = StatusCodes.Status404NotFound;
+    }
+});
+
+
+
+
+
+
+
+
+var context = app.Services.CreateScope().ServiceProvider.GetRequiredService<DataContext>();
+SeedData.SeedDatabase(context);
+/*
+ * app.Services.CreateScope() => Creates a new scope for dependency injection (DI).
+ * .ServiceProvider => the ServiceProvider retrieves a DataContext instance from the DI container.
+ * .GetRequiredService<DataContext>() => Retrieves an instance of DataContext from the service provider.
+ * SeedData.SeedDatabase(context) => Calls the SeedDatabase method in the SeedData class and passes the DataContext instance as a parameter.
+ * */
+
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*RouteValues:- A dictionary-like property (IDictionary<string, object?>) available in the HttpRequest object.
+ * if the route pattern is /products/{id} and the URL /products/42 is requested, 
+ * the RouteValues dictionary will contain { "id", "42" }.
+ * 
+ */
