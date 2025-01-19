109616c1841ed5a185facb651871daf199ab77fd
Shishir-vardhan <shishirvardhan695@gmail.com>
2025-01-19 23:27:50 +0530

    Implemented Database connection+creating table in database

---
+++ Core/Models/Category.cs
@@ -0,0 +1,11 @@
+﻿namespace Core.Data
+{
+    public class Category
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public IEnumerable<Product> Products { get; set; }   
+
+
+    }
+}
--- Core/Program.cs
+++ Core/Program.cs
@@ -1,9 +1,17 @@
+using Core.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System.Data.Common;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<DataContext>(Options =>
+{
+    Options.UseSqlServer(builder.Configuration["ConnectionStrings:DbConnection"]);
+});
 
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
-
-app.UseStaticFiles();
 
 app.Run();
